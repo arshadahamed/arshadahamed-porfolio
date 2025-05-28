c8db9d71b61d765ec5495f22fc87ad80d1025afb
arshadahamed <arshadpayoneer@gmail.com>
2025-05-29 00:43:31 +0530

    Stack Images added

--- src/components/sections/partnersMarquee.jsx
+++ src/components/sections/partnersMarquee.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Marquee from "react-fast-marquee";
 
 const PartnersMarquee = () => {
+    const partnerLogos = Array.from({ length: 15 }, (_, i) => `/images/client-logos/partner${i + 1}.png`);
+
     return (
         <div className="about-content-part-bottom">
             <h2>Tech Stack Expertise</h2>
             <div className="company-list">
                 <div className="scroller">
                     <div className="scroller__inner">
-                        <Marquee>
-                            <img src={"/images/client-logos/partner1.png"} alt="" />
-                            <img src={"/images/client-logos/partner2.png"} alt="" />
-                            <img src={"/images/client-logos/partner3.png"} alt="" />
-                            <img src={"/images/client-logos/partner4.png"} alt="" />
-                            <img src={"/images/client-logos/partner5.png"} alt="" />
+                        <Marquee pauseOnHover speed={50} gradient={false}>
+                            {partnerLogos.map((src, index) => (
+                                <img
+                                    key={index}
+                                    src={src}
+                                    alt={`Partner ${index + 1}`}
+                                    style={{
+                                        height: 30,         // 👈 smaller height
+                                        maxWidth: 100,      // 👈 optional: restrict max width
+                                        objectFit: 'contain',
+                                        marginRight: 10
+                                    }}
+                                />
+                            ))}
                         </Marquee>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default PartnersMarquee
+export default PartnersMarquee;
